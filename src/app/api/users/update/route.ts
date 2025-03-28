7c3a3434b5d9e5c8c658cab3044adba842ff4950
aditya-narayana <aditya.narayan@greenfence.com>
2025-03-28 17:48:19 +0530

    updated with fixes

---
+++ src/app/api/users/update/route.ts
@@ -0,0 +1,33 @@
+import { connect } from "@/lib/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function POST(request: NextRequest) {
+    try {
+      const { email } = await request.json();
+      console.log("Email from update endpointis ", email);
+      
+      const foundUser = await User.findOne({ email });
+      if (!foundUser) {
+        return NextResponse.json({ message: "User not found" }, { status: 400 });
+      }
+  
+      foundUser.count = foundUser.count + 18;
+      await foundUser.save();
+
+      return NextResponse.json(
+        { foundUser, message: "User updated successfully", success: true },
+        { status: 200 },
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+      }
+      return NextResponse.json(
+        { error: "An unknown error occurred" },
+        { status: 500 },
+      );
+    }
+  }
---
+++ src/models/userModel.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  count: {
+    type: Number,
+    required: [true, "Please provide a password"],
+  }
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
